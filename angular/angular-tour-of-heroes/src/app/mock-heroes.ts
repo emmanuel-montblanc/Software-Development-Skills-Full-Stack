0e258c0199ae18098241dadeae16e3769fa6d624
Emmanuel MONTBLANC <emmanuel.montblanc@insa-lyon.fr>
2022-05-12 17:28:28 +0300

    initial commit angular

---
+++ angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Sylvanas' },
+  { id: 12, name: 'Arthas' },
+  { id: 13, name: 'Varian' },
+  { id: 14, name: 'Malfurion' },
+  { id: 15, name: 'Rexxar' },
+  { id: 16, name: 'Ragnaros' },
+  { id: 17, name: 'Ilidan' },
+  { id: 18, name: 'Uther' },
+  { id: 19, name: 'Thrall' },
+  { id: 20, name: 'Saurcro' }
+];
